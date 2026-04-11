1ec44f78228da53c3620108bbe4ff5e19133a676
Frédéric Collonval <frederic.collonval@webscit.com>
2026-04-11 08:30:10 +0200

    feat(registry): add native-select component

---
+++ packages/registry/src/components/native-select/native-select.tsx
@@ -0,0 +1,11 @@
+import "./native-select.css";
+
+export function NativeSelect({ className, ...props }: React.ComponentProps<"select">) {
+  return (
+    <select
+      data-slot="native-select"
+      className={`sct-native-select${className ? ` ${className}` : ""}`}
+      {...props}
+    />
+  );
+}
